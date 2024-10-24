4d92827f0eefb0f0333efa7e5a4add9f27e84ea3
Megagig <megagigsoftwaresolutions.com>
2024-10-24 17:19:24 +0100

    Connection to the Database Established

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 require('express-async-errors');
 const express = require('express');
 const errorHandler = require('./handlers/errorHandler');
+const mongoose = require('mongoose');
+require('dotenv').config();
 const app = express();
+
+// Connect to the Database
+mongoose
+  .connect(process.env.MONGODB_URI, {})
+  .then(() => {
+    console.log('Connected to the database');
+  })
+  .catch((err) => {
+    console.log('Error connecting to the database', err);
+  });
+// Middleware
 
 app.use(express.json());
 
