e3ab19811331396dcc4591b269d0797d0f8303ec
Megagig <megagigsoftwaresolutions.com>
2024-10-24 20:29:55 +0100

    Handling user registration

--- modules/users/controllers/register.js
+++ modules/users/controllers/register.js
@@ -1,6 +1,34 @@
-const register = (req, res) => {
-  res.status(200).json({
-    status: 'From Resgister route',
+const mongoose = require('mongoose');
+
+const register = async (req, res) => {
+  const usersModel = mongoose.model('users');
+  const { name, email, password, confirm_password, balance } = req.body;
+
+  //validation
+
+  if (!name) throw 'Name is required';
+  if (!email) throw 'Email is required';
+  if (!password) throw 'Password is required';
+  if (password !== confirm_password)
+    throw 'Password and confirm password must match';
+  if (password.length < 6) throw 'Password must be at least 6 characters';
+
+  //validation for dupb
+  const getDuplicateEmail = await usersModel.findOne({
+    email: email,
+  });
+
+  if (getDuplicateEmail) throw 'This Email Already Exists';
+
+  await usersModel.create({
+    name: name,
+    email: email,
+    password: password,
+    balance: balance,
+  });
+
+  res.status(201).json({
+    status: 'User Registered Successfully!',
   });
 };
 
