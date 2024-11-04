84ced99e09839f9062fe372b628ecc70f6ef435e
Megagig <megagigsoftwaresolutions.com>
2024-11-04 22:27:11 +0100

    Working with Query string parameters in express

--- modules/transactions/controllers/getTransactions.js
+++ modules/transactions/controllers/getTransactions.js
@@ -3,8 +3,11 @@ const getTransactions = async (req, res) => {
 
 	const transactionsModel = mongoose.model('transactions');
 	
+	console.log(req.query)
+	
 	const transactions = await transactionsModel.find({
 		user_id: req.user.id,
+		...req.query,
 	});
 	res.status(200).json({
 		status: 'success',
