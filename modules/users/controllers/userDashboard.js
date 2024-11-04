2509c9806f1dd908207e6566aab8e509d92dd11a
Megagig <megagigsoftwaresolutions.com>
2024-11-04 23:50:53 +0100

    Finalizing user dashboard

--- modules/users/controllers/userDashboard.js
+++ modules/users/controllers/userDashboard.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const userDashboard = async (req, res) => {
   // Import User model from models folder
   const usersModel = mongoose.model('users');
+  const transactionsModel = mongoose.model('transactions');
   console.log(req.user);
   //Get the user ID from req.user, set by the auth middleware
   // Query the database to find the user with the ID
@@ -10,11 +11,18 @@ const userDashboard = async (req, res) => {
       _id: req.user.id,
     })
     .select('-password'); // Exclude the password from the response
+  
+    // Query the database to find all transactions for the user
+    const transactions = await transactionsModel.find({
+        user_id: req.user.id,
+    }).sort({createdAt: -1}).limit(5) // Sort the transactions by createdAt in descending order and limit to 5
+ 
 
     // Send the user data in the response
   res.status(200).json({
     status: 'Success!',
     data: getUser,
+    transactions: transactions,
   });
 };
 
