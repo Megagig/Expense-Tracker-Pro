36eb34d70d6988773e6437f9003cb22812a95420
Megagig <megagigsoftwaresolutions.com>
2024-11-06 08:41:28 +0100

    Validate transaction_id format

--- modules/transactions/controllers/deleteTransaction.js
+++ modules/transactions/controllers/deleteTransaction.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 const deleteTransaction = async (req, res) => {
 	const transactionsModel = mongoose.model('transactions');
 	  const { id } = req.params;
+	  
+	  if (!validator.isMongoId(id.toString())) throw 'please provide a valid  transaction id';
   const transaction = await transactionsModel.findById(id);
   if (!transaction) {
 	return res.status(404).json({ error: 'Transaction not found' });
